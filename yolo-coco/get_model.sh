bb6081e4449c5de0b418e0276d7e779079b4f39c
Arunava <learningdroidarunava@gmail.com>
2018-11-18 23:19:05 +0530

    Added the yolov3 model weights and configuration files

---
+++ yolo-coco/get_model.sh
@@ -0,0 +1,9 @@
+# The model here is YOLOv3 model trained by the official
+# authors of the model using the DarkNet Framework
+# and is made available from their website
+# http://pjreddie.com/yolo/
+
+echo 'Getting the YOLOv3 model'
+echo 'Starting Download...'
+wget --no-check-certificate https://pjreddie.com/media/files/yolov3.weights
+echo 'Download completed successfully!!'
