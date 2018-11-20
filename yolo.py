278a3dfd4fad86cf7cbbb9028795e18bb6fb62e8
iarunava <learningdroidarunava@gmail.com>
2018-11-20 05:51:08 +0530

    Now added code to perform inference on the images

--- yolo.py
+++ yolo.py
@@ -2,7 +2,67 @@ import numpy as np
 import argparse
 import cv2 as cv
 import subprocess
+import time
 import os
+
+FLAGS = []
+
+def show_image(img):
+    cv.imshow("Image", img)
+    cv.waitKey(0)
+
+def draw_labels_and_boxes(img, boxes, confidences, classids, idxs, colors, labels):
+    # If there are any detections
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            # Get the bounding box coordinates
+            x, y = boxes[i][0], boxes[i][1]
+            w, h = boxes[i][2], boxes[i][3]
+            
+            # Get the unique color for this class
+            color = [int(c) for c in COLORS[classids[i]]]
+
+            # Draw the bounding box rectangle and label on the image
+            cv.rectangle(img, (x, y), (x+w, y+h), color, 2)
+            text = "{}: {:4f}".format(labels[classids[i]], confidences[i])
+            cv.putText(img, text, (x, y-5), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    return img
+
+
+
+def generate_boxes_confidences_classids(outs, height, width):
+    boxes = []
+    confidences = []
+    classids = []
+
+    for out in outs:
+        for detection in out:
+            print (detection)
+            
+            # Get the scores, classid, and the confidence of the prediction
+            scores = detection[:5]
+            classid = np.argmax(scores)
+            confidence = scores[classid]
+            
+            # Consider only the predictions that are above a certain confidence level
+            if confidence > FLAGS.confidence:
+                # TODO Check detection
+                box = detection[0:4] * np.array([height, width, height, width])
+                (centerX, centerY, bwidth, bheight) = box.astype('int')
+
+                # Using the center x, y coordinates to derive the top
+                # and the left corner of the bounding box
+                x = int(centerX - (bwidth / 2))
+                y = int(centerY - (bheight / 2))
+
+                # Append to list
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classids.append(classid)
+
+    return boxes, confidences, classids
+                
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -53,6 +113,11 @@ if __name__ == '__main__':
 		help='Set to True, if the model weights and configurations \
 				are not present on your local machine.')
 
+        parser.add_argument('-t', '--show-time',
+                type=bool,
+                default=False,
+                help='Show the time taken to infer each image.')
+
 	FLAGS, unparsed = parser.parse_known_args()
 
 	# Download the YOLOv3 models if needed
@@ -77,9 +142,39 @@ if __name__ == '__main__':
 		# Read the image
 		try:
 			img = cv.imread(FLAGS.image_path)
+                        height, width = img.shape[:2]
 		except:
 			raise 'Image cannot be loaded!\n\
-				   Please check the path provided!'
+                               Please check the path provided!'
+
+                finally:
+                    # Contructing a blob from the input image
+                    blob = cv.dnn.blobFromImage(img, 1 / 255.0, (416, 416), 
+                                    swapRB=True, crop=False)
+
+                    # Perform a forward pass of the YOLO object detector
+                    net.setInput(blob)
+
+                    # Getting the outputs from the output layers
+                    start = time.time()
+                    outs = net.forward(layer_names)
+                    end = time.time()
+
+                    if FLAGS.show_time:
+                        print ("[INFO] YOLOv3 took {:6f} seconds".format(end - start))
+
+                    
+                    # Generate the boxes, confidences, and classIDs
+                    boxes, confidences, classids = generate_boxes_confidences_classids(outs, height, width)
+                    
+                    # Apply Non-Maxima Suppression to suppress overlapping bounding boxes
+                    idxs = cv.dnn.NMSBoxes(boxes, confidence, FLAGS.confidence, FLAGS.threshold)
+
+                    # Draw labels and boxes on the image
+                    img = draw_labels_and_boxes(img, boxes, confidences, classids, idxs, colors)
+
+                    # show the image
+                    show_image(img)
 
 	else:
 		pass
